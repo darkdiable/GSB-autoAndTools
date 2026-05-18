9a75cde382c1da4bb4bb1ee166aaa9027ffb3c0d
bilei <darkdiable@163.com>
2026-05-18 18:28:45 +0800

    fix bug : 生成的epub电子书无法打开，提示"格式不正确"。请检查并修复这个问题。

--- image2Ebook/src/epub_builder.py
+++ image2Ebook/src/epub_builder.py
@@ -28,7 +28,7 @@ class EpubBuilder:
 
     def _write_mimetype(self):
         mimetype_path = self.temp_dir / "mimetype"
-        with open(mimetype_path, 'w', encoding='utf-8') as f:
+        with open(mimetype_path, 'w', encoding='utf-8', newline='') as f:
             f.write("application/epub+zip")
 
     def _write_container_xml(self):
@@ -47,9 +47,11 @@ class EpubBuilder:
   padding: 0;
 }
 
-body {
+html, body {
   margin: 0;
   padding: 0;
+  width: 100%;
+  height: 100%;
   background-color: #FFFFFF;
 }
 
@@ -62,34 +64,77 @@ img {
 }
 
 .page {
+  text-align: center;
+  margin: 0;
+  padding: 0;
   width: 100%;
   height: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   page-break-after: always;
 }
 '''
         with open(self.epub_dir / "style" / "main.css", 'w', encoding='utf-8') as f:
             f.write(css_content)
 
-    def _write_xhtml_page(self, page_name: str, image_path: str, is_cover: bool = False):
+    def _write_xhtml_page(self, page_name: str, image_path: str, page_title: str, is_cover: bool = False):
         xhtml_content = f'''<?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:epub="http://www.idpf.org/2007/ops">
 <head>
-  <title>{self.book_title}</title>
-  <meta charset="UTF-8"/>
+  <title>{page_title}</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
   <link rel="stylesheet" type="text/css" href="../style/main.css"/>
 </head>
 <body>
-  <div class="page" {'epub:type="cover"' if is_cover else ''}>
-    <img src="../images/{os.path.basename(image_path)}" alt="{'Cover' if is_cover else 'Page'}"/>
+  <div class="page">
+    <img src="../images/{os.path.basename(image_path)}" alt="{page_title}"/>
   </div>
 </body>
 </html>'''
         with open(self.epub_dir / "text" / page_name, 'w', encoding='utf-8') as f:
             f.write(xhtml_content)
+
+    def _write_toc_ncx(self, cover_image: Optional[str], content_images: List[str]):
+        nav_points = []
+        play_order = 1
+        
+        if cover_image:
+            nav_points.append(f'''    <navPoint id="nav-{play_order}" playOrder="{play_order}">
+      <navLabel>
+        <text>封面</text>
+      </navLabel>
+      <content src="text/cover.xhtml"/>
+    </navPoint>''')
+            play_order += 1
+        
+        for i, img in enumerate(content_images):
+            page_num = i + 1
+            nav_points.append(f'''    <navPoint id="nav-{play_order}" playOrder="{play_order}">
+      <navLabel>
+        <text>第 {page_num} 页</text>
+      </navLabel>
+      <content src="text/page{page_num:04d}.xhtml"/>
+    </navPoint>''')
+            play_order += 1
+        
+        nav_points_str = '\n'.join(nav_points)
+        
+        ncx_content = f'''<?xml version="1.0" encoding="UTF-8"?>
+<ncx xmlns="http://www.daisy.org/z3986/2005/ncx/" version="2005-1">
+  <head>
+    <meta name="dtb:uid" content="{self.book_id}"/>
+    <meta name="dtb:depth" content="1"/>
+    <meta name="dtb:totalPageCount" content="0"/>
+    <meta name="dtb:maxPageNumber" content="0"/>
+  </head>
+  <docTitle>
+    <text>{self.book_title}</text>
+  </docTitle>
+  <navMap>
+{nav_points_str}
+  </navMap>
+</ncx>'''
+        with open(self.epub_dir / "toc.ncx", 'w', encoding='utf-8') as f:
+            f.write(ncx_content)
 
     def _write_content_opf(self, cover_image: Optional[str], content_images: List[str]):
         modified_date = datetime.now(timezone.utc).strftime('%Y-%m-%dT%H:%M:%SZ')
@@ -97,6 +142,7 @@ img {
         manifest_items = []
         spine_items = []
         
+        manifest_items.append('<item id="ncx" href="toc.ncx" media-type="application/x-dtbncx+xml"/>')
         manifest_items.append('<item id="style" href="style/main.css" media-type="text/css"/>')
         
         if cover_image:
@@ -122,11 +168,12 @@ img {
     <dc:creator>{self.author}</dc:creator>
     <dc:language>zh-CN</dc:language>
     <meta property="dcterms:modified">{modified_date}</meta>
+    <meta name="cover" content="cover-image"/>
   </metadata>
   <manifest>
     {manifest_str}
   </manifest>
-  <spine>
+  <spine toc="ncx">
     {spine_str}
   </spine>
 </package>'''
@@ -159,27 +206,40 @@ img {
 
     def _create_xhtml_pages(self, cover_image: Optional[str], content_images: List[str]):
         if cover_image:
-            self._write_xhtml_page("cover.xhtml", cover_image, is_cover=True)
+            self._write_xhtml_page("cover.xhtml", cover_image, "封面", is_cover=True)
         
         for i, img in enumerate(content_images):
             page_name = f"page{i+1:04d}.xhtml"
-            self._write_xhtml_page(page_name, img, is_cover=False)
+            page_title = f"第 {i+1} 页"
+            self._write_xhtml_page(page_name, img, page_title, is_cover=False)
 
     def _create_epub_zip(self, output_filename: str) -> str:
         epub_path = self.output_dir / output_filename
         if epub_path.exists():
             epub_path.unlink()
         
+        priority_files = [
+            ("mimetype", zipfile.ZIP_STORED),
+            ("META-INF/container.xml", zipfile.ZIP_DEFLATED),
+        ]
+        
         with zipfile.ZipFile(epub_path, 'w', zipfile.ZIP_DEFLATED) as zf:
-            zf.write(self.temp_dir / "mimetype", "mimetype", compress_type=zipfile.ZIP_STORED)
+            for rel_path, compress_type in priority_files:
+                zf.write(self.temp_dir / rel_path, rel_path, compress_type=compress_type)
             
+            all_files = []
             for root, dirs, files in os.walk(self.temp_dir):
                 for file in files:
-                    if file == "mimetype":
-                        continue
                     file_path = Path(root) / file
                     arcname = str(file_path.relative_to(self.temp_dir))
-                    zf.write(file_path, arcname)
+                    if arcname in [f[0] for f in priority_files]:
+                        continue
+                    all_files.append((file_path, arcname))
+            
+            all_files.sort(key=lambda x: x[1])
+            
+            for file_path, arcname in all_files:
+                zf.write(file_path, arcname)
         
         return str(epub_path)
 
@@ -195,6 +255,7 @@ img {
             self._write_css()
             self._copy_images(cover_image, content_images, img_dir)
             self._create_xhtml_pages(cover_image, content_images)
+            self._write_toc_ncx(cover_image, content_images)
             self._write_content_opf(cover_image, content_images)
             epub_path = self._create_epub_zip(output_filename)
             return epub_path
