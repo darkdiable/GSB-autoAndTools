63957f5ccea992b73b25c18a3818e601b01bf432
bilei <darkdiable@163.com>
2026-06-02 18:52:53 +0800

    fix bug

--- httpServer/src/server.py
+++ httpServer/src/server.py
@@ -32,6 +32,52 @@ class StaticFileHandler(SimpleHTTPRequestHandler):
         """猜测文件MIME类型"""
         mimetype, _ = mimetypes.guess_type(path)
         return mimetype or 'application/octet-stream'
+    
+    def send_head(self):
+        """发送响应头，修复中文编码问题"""
+        path = self.translate_path(self.path)
+        f = None
+        
+        # 检查路径是否为目录
+        if os.path.isdir(path):
+            if not self.path.endswith('/'):
+                self.send_response(301)
+                self.send_header('Location', self.path + '/')
+                self.end_headers()
+                return None
+            for index in ['index.html', 'index.htm']:
+                index_path = os.path.join(path, index)
+                if os.path.exists(index_path):
+                    path = index_path
+                    break
+            else:
+                return self.list_directory(path)
+        
+        # 获取文件MIME类型
+        ctype = self.guess_type(path)
+        
+        # 检查文件是否存在且可读
+        try:
+            f = open(path, 'rb')
+        except OSError:
+            self.send_error(404, "File not found")
+            return None
+        
+        try:
+            self.send_response(200)
+            # 为文本类型添加UTF-8字符集，解决中文乱码问题
+            if ctype.startswith('text/') or ctype == 'application/json' or ctype == 'application/javascript':
+                self.send_header('Content-Type', f'{ctype}; charset=utf-8')
+            else:
+                self.send_header('Content-Type', ctype)
+            fs = os.fstat(f.fileno())
+            self.send_header('Content-Length', str(fs.st_size))
+            self.send_header('Last-Modified', self.date_time_string(fs.st_mtime))
+            self.end_headers()
+            return f
+        except:
+            f.close()
+            raise
 
 
 class SimpleHttpServer:
